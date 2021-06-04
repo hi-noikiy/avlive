4f099412ac17373f8404976c0414cbd3bf0ffcb6
李威臣 <1685629968@qq.com>
2021-06-04 19:08:14 +0800

    nvue层级问题

--- pages/tool/video_live/look.vue
+++ pages/tool/video_live/look.vue
@@ -1,28 +1,23 @@
 <template>
 	<view class="main" @touchmove.stop.prevent="moveHandle">
-		<video class="live"
-			src="https://mp4.vjshi.com/2021-05-19/e141f65baf6e46b4dd384fafbb5a6274.mp4"
-			:autoplay="true"
-			:loop="true"
-			:controls="false"
-			:enable-progress-gesture="false"
-			:show-center-play-btn="false"
-			:show-play-btn="false"
-			:show-fullscreen-btn="false"
-			object-fit="fill">
-			<cover-view class="author" style="width: 500rpx;height: 500rpx;background-color: #008000;">
-				简单的测试
-				<view>测试11</view>
-				<text>测试2</text>
-				<!-- <image src="../../users/static/1.png"></image>
-				<view class="info">
-					<view class="t">王保保</view>
-					<view class="b">432本场点赞</view>
-				</view>
-				<view class="btn">关注</view> -->
+		<video class="live" src="https://mp4.vjshi.com/2021-05-19/e141f65baf6e46b4dd384fafbb5a6274.mp4" :autoplay="true"
+			:loop="true" :controls="false" :enable-progress-gesture="false" :show-center-play-btn="false"
+			:show-play-btn="false" :show-fullscreen-btn="false" object-fit="fill">
+			<cover-view class="center"
+				style="width: 209rpx;height: 60rpx;background-color: #743180;float: left;margin-left: 62rpx;margin-top: 41rpx;opacity: 0.2;">
+
 			</cover-view>
-			<cover-view>测试41</cover-view>
+			<cover-view class="headimg"
+				style="width: 60rpx;height: 60rpx;background-color: #6c7780;border-radius: 50%;margin-left: -232rpx;float: left;margin-top: 41rpx;">
+
+			</cover-view>
+			<cover-view class="right"
+				style="width: 80rpx;height: 60rpx;background-color: #ff0000;border-radius: 30rpx;margin-left: -42rpx;float: left;margin-top: 41rpx;text-align: center;line-height: 60rpx;color: #FFFFFF;">
+				关注
+			</cover-view>
+
 		</video>
+		<!-- <video src="https://mp4.vjshi.com/2021-05-19/e141f65baf6e46b4dd384fafbb5a6274.mp4"></video> -->
 	</view>
 </template>
 
@@ -30,8 +25,12 @@
 	export default {
 		data() {
 			return {
-				
+
 			}
+		},
+		onShow() {
+			const subNvue=uni.getSubNVueById('popup');   //获取 
+			subNvue.show()  // 显示
 		},
 		methods: {
 			moveHandle() {
@@ -46,10 +45,9 @@
 		width: 750rpx;
 		height: 100vh;
 	}
+
 	.live {
-		position: relative;
-		z-index: 1;
-		width: 100%;
-		height: 100%;
+		width: 750rpx;
+		height: 100vh;
 	}
 </style>
